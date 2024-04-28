7f4c48acf2849c80b3f1bc83c98293535467847f
Aaron Hebson <Aarommh7@gmail.com>
2024-04-28 17:38:16 -0400

    Set practice clean up

--- 07_Day_Sets/day_7/Exercises_Level1.py
+++ 07_Day_Sets/day_7/Exercises_Level1.py
@@ -4,9 +4,6 @@
 ########################################
 
 it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
-A = {19, 22, 24, 20, 25, 26}
-B = {19, 22, 20, 25, 26, 24, 28, 27}
-age = [22, 19, 24, 25, 26, 24, 25, 24]
 
 print(f"IT companies set is:\n{it_companies}")
 print(f"\nThere are a total of {len(it_companies)} IT companies in the set.")
